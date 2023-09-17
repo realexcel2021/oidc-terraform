da3410ace4d98c1ec9b8ba24ecf1856e7e43ef5c
Sheriff <realexcel2020@gmail.com>
2023-09-18 00:20:22 +0100

    Added the key pair

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,13 +12,35 @@ provider "aws" {
 }
 
 
+data "aws_key_pair" "web-key-pair" {
+  key_name           = var.key_pair_name
+  include_public_key = true
+}
+
 # Web server
 
 resource "aws_instance" "nginx" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
-
+    security_groups = [ aws_security_group.nginx_sg.name ]
+    key_name = data.aws_key_pair.web-key-pair.key_name
     tags = { 
     Name  = "NGINX"
   }
+}
+
+# Security group for the web server
+
+resource "aws_security_group" "nginx_sg" {
+    name = "Allow Public Access"
+
+    ingress {
+      from_port = 8080
+      to_port = 8080 
+      cidr_blocks = ["0.0.0.0/0"]   
+    }
+
+    tags = {
+      Name = "Nginx Web Server-sg"
+    }
 }
---
+++ terraform/var.tf
@@ -0,0 +1,5 @@
+variable "key_pair_name" {
+    # Kindly name the key to the respective name you gave your key pair
+    default = "web-server-key"
+}
+
