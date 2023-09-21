9c52d0a61967b9651c4fd502e1136571db3f2f59
Sheriff <realexcel2020@gmail.com>
2023-09-21 08:53:26 +0100

    Made clean ups

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,39 +8,50 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
-}
-
-
-data "aws_key_pair" "web-key-pair" {
-  key_name           = var.key_pair_name
-  include_public_key = true
+  region = "us-east-1"
 }
 
 # Web server
 
 resource "aws_instance" "nginx" {
-    ami = "ami-053b0d53c279acc90"
-    instance_type = "t2.micro"
-    security_groups = [ aws_security_group.nginx_sg.name ]
-    key_name = data.aws_key_pair.web-key-pair.key_name
-    tags = { 
-    Name  = "NGINX"
+  ami             = "ami-053b0d53c279acc90"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.nginx_sg.name]
+  key_name        = data.aws_key_pair.web-key-pair.key_name
+  user_data       = file("./script.sh")
+
+  tags = {
+    Name = "NGINX"
   }
 }
 
 # Security group for the web server
 
 resource "aws_security_group" "nginx_sg" {
-    name = "Allow Public Access"
+  name = "Allow Public Access on ssh and web server"
 
-    ingress {
-      from_port = 8080
-      to_port = 8080 
-      cidr_blocks = ["0.0.0.0/0"]   
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-      Name = "Nginx Web Server-sg"
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags = {
+    Name = "Nginx Web Server-sg"
+  }
 }
--- terraform/var.tf
+++
@@ -1,5 +0,0 @@
-variable "key_pair_name" {
-    # Kindly name the key to the respective name you gave your key pair
-    default = "web-server-key"
-}
-
